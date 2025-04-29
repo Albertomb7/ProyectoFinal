5c777893a5b1aa449642c6fb96fd8f7d5da55fb1
Alberto7 <amoralesb24@miumg.edu.gt>
2025-04-29 13:45:44 -0600

    Actualizacion de log in

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,76 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbl_inicio = new System.Windows.Forms.Label();
             this.txt_user = new System.Windows.Forms.TextBox();
             this.txt_password = new System.Windows.Forms.TextBox();
             this.lbl_user = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_inicio
             // 
             this.lbl_inicio.AutoSize = true;
-            this.lbl_inicio.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_inicio.Location = new System.Drawing.Point(175, 68);
+            this.lbl_inicio.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_inicio.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_inicio.ForeColor = System.Drawing.Color.White;
+            this.lbl_inicio.Location = new System.Drawing.Point(98, 75);
             this.lbl_inicio.Name = "lbl_inicio";
-            this.lbl_inicio.Size = new System.Drawing.Size(220, 34);
+            this.lbl_inicio.Size = new System.Drawing.Size(94, 24);
             this.lbl_inicio.TabIndex = 0;
-            this.lbl_inicio.Text = "INICIO DE SESION";
+            this.lbl_inicio.Text = "LOG IN ";
+            this.lbl_inicio.Click += new System.EventHandler(this.lbl_inicio_Click);
             // 
             // txt_user
             // 
-            this.txt_user.Location = new System.Drawing.Point(177, 142);
+            this.txt_user.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            this.txt_user.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_user.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_user.ForeColor = System.Drawing.Color.White;
+            this.txt_user.Location = new System.Drawing.Point(44, 160);
             this.txt_user.Name = "txt_user";
-            this.txt_user.Size = new System.Drawing.Size(208, 20);
+            this.txt_user.Size = new System.Drawing.Size(208, 19);
             this.txt_user.TabIndex = 1;
             this.txt_user.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(181, 211);
+            this.txt_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            this.txt_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_password.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F);
+            this.txt_password.ForeColor = System.Drawing.Color.White;
+            this.txt_password.Location = new System.Drawing.Point(44, 226);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(208, 20);
+            this.txt_password.Size = new System.Drawing.Size(207, 19);
             this.txt_password.TabIndex = 2;
             this.txt_password.TextChanged += new System.EventHandler(this.txt_password_TextChanged);
             // 
             // lbl_user
             // 
             this.lbl_user.AutoSize = true;
-            this.lbl_user.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_user.Location = new System.Drawing.Point(177, 117);
+            this.lbl_user.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_user.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_user.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbl_user.Location = new System.Drawing.Point(40, 131);
             this.lbl_user.Name = "lbl_user";
-            this.lbl_user.Size = new System.Drawing.Size(63, 19);
+            this.lbl_user.Size = new System.Drawing.Size(72, 19);
             this.lbl_user.TabIndex = 3;
             this.lbl_user.Text = "Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.label2.Location = new System.Drawing.Point(177, 189);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.Location = new System.Drawing.Point(40, 196);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(88, 19);
+            this.label2.Size = new System.Drawing.Size(105, 19);
             this.label2.TabIndex = 4;
             this.label2.Text = "Contraseña";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.ForeColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(44, 247);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 1);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(27)))), ((int)(((byte)(34)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.lbl_inicio);
+            this.panel2.Controls.Add(this.txt_password);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.txt_user);
+            this.panel2.Controls.Add(this.lbl_user);
+            this.panel2.Location = new System.Drawing.Point(180, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(292, 370);
+            this.panel2.TabIndex = 6;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel3.ForeColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(44, 181);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(207, 1);
+            this.panel3.TabIndex = 6;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(134)))), ((int)(((byte)(227)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(44, 283);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 27);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Log in ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(96, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 46);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel4.ForeColor = System.Drawing.Color.Black;
+            this.panel4.Location = new System.Drawing.Point(13, 112);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(264, 1);
+            this.panel4.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Menu;
-            this.ClientSize = new System.Drawing.Size(541, 314);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.lbl_user);
-            this.Controls.Add(this.txt_password);
-            this.Controls.Add(this.txt_user);
-            this.Controls.Add(this.lbl_inicio);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+            this.ClientSize = new System.Drawing.Size(657, 429);
+            this.Controls.Add(this.panel2);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -108,6 +208,12 @@
         private System.Windows.Forms.TextBox txt_password;
         private System.Windows.Forms.Label lbl_user;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel4;
     }
 }
 
