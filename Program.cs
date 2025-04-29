46d7cc477fe39d030b51a7c5482a3eb53e14f1f7
Romanmin1614 <salasroman280@gmail.com>
2025-04-28 23:18:16 -0600

    prueba 2

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace ProyectoFinal
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            
+         
         }
     }
 }
