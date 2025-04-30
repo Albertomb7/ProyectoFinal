e6be88026ad840e854ea8b074d4138aef820b3f1
Lanube2024 <[200~marvincabrera2425@gmail.com~>
2025-04-29 21:59:44 -0600

    cconexion a la base de datos usando el paqute nuget Microsoft Data SQL client

---
+++ conexionSql.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace ProyectoFinal
+{
+    internal class conexionSql
+    {
+        public static void ObtenerConexion()
+        {
+            Microsoft.Data.SqlClient.SqlConnection conexion = new Microsoft.Data.SqlClient.SqlConnection("Server=tcp:mcabrera23.database.windows.net,1433;Initial Catalog=proyectoFinalProgra1;Persist Security Info=False;User ID=administrador;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            conexion.Open();
+        }
+    }
+}
