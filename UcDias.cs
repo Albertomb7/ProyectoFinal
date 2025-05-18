0fb253e99a99b577405ee9e01ce7fc1d2609031a
Romanmin1614 <salasroman280@gmail.com>
2025-05-18 09:29:37 -0600

    notifiacion de burbujas

--- UcDias.cs
+++ UcDias.cs
@@ -9,7 +9,7 @@ namespace CalendarioApp
         private string _dia;
         private bool _tieneEvento;
         private Color? _colorPersonalizado;
-        private Color colorBaseActual = Color.FromArgb(30, 30, 40); // Color base por defecto
+        private Color colorBaseActual = Color.FromArgb(30, 30, 40); // Color base por defecto // R,S.
 
         public DateTime FechaCelda { get; set; }
 
@@ -65,9 +65,10 @@ namespace CalendarioApp
 
         private void AplicarColores()
         {
-            colorBaseActual = _colorPersonalizado ?? Color.FromArgb(30, 30, 40);
-            this.BackColor = colorBaseActual;
-            this.Invalidate();
+            // Siempre usamos el fondo oscuro, sin pintar por el evento // R,S.
+            colorBaseActual = Color.FromArgb(30, 30, 40); // R,S.
+            this.BackColor = colorBaseActual; // R,S.
+            this.Invalidate(); // Para que se dibuje el círculo si tiene evento // R,S.
         }
 
         public event EventHandler DiaClickeado;
@@ -82,19 +83,9 @@ namespace CalendarioApp
 
         private void lblDia_Click(object sender, EventArgs e)
         {
-            UcDias_Click(sender, e);           
+            UcDias_Click(sender, e);
         }
 
-        //no tocar----- los // fueron remplanzados
-        //private void UcDias_MouseEnter(object sender, EventArgs e)
-        //{
-        //  this.BackColor = Color.FromArgb(60, 63, 70); // color resaltado al pasar el mouse
-        //}
-
-        //private void UcDias_MouseLeave(object sender, EventArgs e)
-        //{
-        //   this.BackColor = colorBaseActual; // volver al color original
-        //}
         private void UcDias_MouseEnter(object sender, EventArgs e)
         {
             this.BackColor = Color.FromArgb(60, 63, 70); // solo cambia visualmente
@@ -105,15 +96,6 @@ namespace CalendarioApp
             this.BackColor = colorBaseActual; // restaura el color personalizado
         }
 
-        //private void lblDia_MouseMove(object sender, MouseEventArgs e)
-        //{
-        //  this.BackColor = Color.FromArgb(60, 63, 70); // mismo color de resaltado
-        //}
-
-        //private void lblDia_MouseLeave(object sender, EventArgs e)
-        //{
-        //   this.BackColor = colorBaseActual;
-        //}
         private void lblDia_MouseMove(object sender, MouseEventArgs e)
         {
             this.BackColor = Color.FromArgb(60, 63, 70); // efecto hover
@@ -124,9 +106,25 @@ namespace CalendarioApp
             this.BackColor = colorBaseActual; // vuelve al color original
         }
 
+        //NUEVO MÉTODO: Dibuja un círculo en la esquina si el día tiene evento // R,S.
+        protected override void OnPaint(PaintEventArgs e) 
+        {
+            base.OnPaint(e); // R,S.
 
+            if (TieneEvento && ColorPersonalizado.HasValue) 
+            {
+                int tamañoCirculo = 12; 
+                int margen = 6; 
+                Color colorCirculo = ColorPersonalizado.Value; 
 
+                using (SolidBrush brush = new SolidBrush(colorCirculo)) 
+                {
+                    int x = this.Width - tamañoCirculo - margen;
+                    int y = margen; //Esto coloca en la parte superior el punto// R,S.
+                    e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; 
+                    e.Graphics.FillEllipse(brush, x, y, tamañoCirculo, tamañoCirculo); 
+                }
+            }
+        }
     }
 }
-
-
